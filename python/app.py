5916060bf5054eef82ae19c925f4901f2b1b981d
rajesh-ramalingam04 <rrajesh04@outlook.com>
2026-02-22 00:16:27 +0530

    v1

--- python/app.py
+++ python/app.py
@@ -1,2 +1,10 @@
-print("Hello from Python Docker container 🚀")
-app.run(host="0.0.0.0", port=5000)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Python Docker container 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
