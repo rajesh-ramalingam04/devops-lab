b38212108c2c6e09208b1f2c3624cc4467ab9572
rajesh-ramalingam04 <rrajesh04@outlook.com>
2026-02-23 00:47:36 +0530

    add

--- python/python-lab/hello.py
+++ python/python-lab/hello.py
@@ -1,2 +1,19 @@
+#used normal print for console view
 print("Hello Rajesh")
 print("Welcome to Python Lab")
+
+
+#Used flask for website view 
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return """
+#     <h1>Hello Rajesh</h1>
+#     <p>Welcome to Python Lab</p>
+#     """
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000, debug=False)
